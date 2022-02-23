6621f0c2fca1a1f401f06da46d854b091058f9b3
superdev905 <superdev905@gmail.com>
2022-02-23 12:24:38 -0300

    first commit

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,9 @@ COPY ./requirements.txt /requirements.txt
 RUN pip install -r /requirements.txt
 
 COPY ./app /app
+COPY ./alembic.ini /alembic.ini
+COPY ./.env /.env
+COPY ./config.cfg /config.cfg
 
 EXPOSE 80:80
+EXPOSE 5191:5191
---
+++ azure_push.sh
@@ -0,0 +1,7 @@
+#/bin/bash!
+docker context use default
+docker-compose --file docker-compose.test.yml up --build
+docker tag vivienda-service_vivienda-api-test cchcdev.azurecr.io/vivienda-service:latest
+docker push cchcdev.azurecr.io/vivienda-service:latest
+docker context use azurecontext
+docker compose --file docker-compose.azure.yml up --build
