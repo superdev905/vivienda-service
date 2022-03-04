97e3b174f1bd99f3482cafc5a9395b546ed00ed1
superdev905 <superdev905@gmail.com>
2022-03-04 00:33:33 -0300

    Pool Service

--- app/database/main.py
+++ app/database/main.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.settings import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=500)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=100)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
