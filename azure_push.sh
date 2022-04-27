cc365e98726fb1e01e1fcf4e8eccf0b0d1263b23
superdev905 <superdev905@gmail.com>
2022-04-27 10:45:37 -0400

    change azure context

--- azure_push.sh
+++ azure_push.sh
@@ -3,5 +3,5 @@ docker context use default
 docker-compose --file docker-compose.test.yml up --build
 docker tag vivienda-service_vivienda-api-test cchcdev.azurecr.io/vivienda-service:latest
 docker push cchcdev.azurecr.io/vivienda-service:latest
-docker context use azurecontext
+docker context use azuretest2
 docker compose --file docker-compose.azure.yml up --build
